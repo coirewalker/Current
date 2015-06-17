1f689e5412e85cc501e58e12dd4f186096970797
whereiscoire <whereiscoire@gmail.com>
2015-06-17 17:03:29 -0400

    connected jambase and spotify API

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-	
+
 	def home
 
 	end
@@ -12,7 +12,27 @@ class HomeController < ApplicationController
 		uri = URI("http://api.jambase.com/events?zipCode=#{users_zip}&radius=10&startDate=#{start_time}&endDate=#{end_time}&page=0&api_key=pedaafhf7xkuaegea2nmrmjs")
 		response = JSON.parse(Net::HTTP.get(uri))
 		@eligible_local_artist = response["Events"]
-		puts response
-		puts uri
-  end
+		puts @eligible_local_artist
+		# puts uri
+		spotify_artist
+	end
+
+private
+	def spotify_artist
+		name = @eligible_local_artist[0]["Artists"][0]["Name"]
+		puts "********** #{name} **********"
+		@playlist = RSpotify::Artist.search(CGI::escape name)	
+	end
+
+	def spotify_complile_playlist
+	 	@playlist.each do |track|
+	 		track[""] #array...
+	 	end
+	end
+
+	def spotify_complete_playlist
+		@spotify_complile_playlist = params[:spotify_complile_playlist]
+		# uri = URI(spotify playlist uri...)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'artists/index'
+
+  get 'artists/show'
+
   root 'home#index'
  
   get '/index', to: 'home#index'
