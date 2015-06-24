01c0090a64d8c3a2a72b055f8441004930de0e66
whereiscoire <whereiscoire@gmail.com>
2015-06-24 16:33:55 -0400

    added controls for player

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-
 $(document).ready(function () {
 
   var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
@@ -28,8 +27,8 @@ $(document).ready(function () {
   //var frequencyData = new Uint8Array(analyser.frequencyBinCount);
   var frequencyData = new Uint8Array(200);
 
-  var svgHeight = '150';
-  var svgWidth = '2000';
+  var svgHeight = '100';
+  var svgWidth = '1600';
   var barPadding = '4';
 
   function createSvg(parent, height, width) {
@@ -69,58 +68,12 @@ $(document).ready(function () {
         });
 
   }
-
   // Run the loop
   renderChart();
+
 
 });
 
 
-
-// $( document ).ready(function() {
-//     console.log( "ready!" );
-// // =====================WEB AUDIO API EQUALIZER ========================
-
-//   // Create a new instance of an audio object and adjust some of its properties
-//   var audio = new Audio();
-//   audio.src = 'Along-The-Coastline.mp3';
-//   audio.controls = true;
-//   audio.loop = true;
-//   audio.autoplay = true;
-//   // Establish all variables that your Analyser will use
-//   var canvas, ctx, source, context, analyser, fbc_array, bars, bar_x, bar_width, bar_height;
-//   // Initialize the MP3 player after the page loads all of its HTML into the window
-//   window.addEventListener("load", initMp3Player, false);
-//   function initMp3Player(){
-//     document.getElementById('audio_box').appendChild(audio);
-//     context = new AudioContext(); // AudioContext object instance
-//     analyser = context.createAnalyser(); // AnalyserNode method
-//     canvas = document.getElementById('analyser_render');
-//     ctx = canvas.getContext('2d');
-//     // Re-route audio playback into the processing graph of the AudioContext
-//     source = context.createMediaElementSource(audio); 
-//     source.connect(analyser);
-//     analyser.connect(context.destination);
-//     frameLooper();
-//   }
-
-//   // frameLooper() animates any style of graphics you wish to the audio frequency
-//   // Looping at the default frame rate that the browser provides(approx. 60 FPS)
-//   function frameLooper(){
-//     window.requestAnimationFrame(frameLooper);
-//     fbc_array = new Uint8Array(analyser.frequencyBinCount);
-//     analyser.getByteFrequencyData(fbc_array);
-//     ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
-//     ctx.fillStyle = '#00CCFF'; // Color of the bars
-//     bars = 100;
-//     for (var i = 0; i < bars; i++) {
-//       bar_x = i * 3;
-//       bar_width = 2;
-//       bar_height = -(fbc_array[i] / 2);
-//       //  fillRect( x, y, width, height ) // Explanation of the parameters below
-//       ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
-//     }
-//   };
-// });
 
 
--- app/models/song.rb
+++ app/models/song.rb
@@ -3,7 +3,7 @@ class Song < ActiveRecord::Base
 
 
 	def sc_player
-	"<iframe width='400px' height='80px' scrolling='no' frameborder='no' src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/#{self.soundcloud_id}&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true'></iframe>"
+	"<iframe width='400px' height='80px' scrolling='no' frameborder='no' src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/#{self.soundcloud_id}&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true'></iframe>"
   end
 
 end
