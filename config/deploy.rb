0229d3085308181dfb42e3dbf312930faffb2181
whereiscoire <whereiscoire@gmail.com>
2015-07-22 23:06:32 -0400

    fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.0'
 
 set :application, 'fourforty'
-set :repo_url, 'git@github.com:zoras/fourforty.git'
+set :repo_url, 'git@github.com:coirewalker/fourforty.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
